21975bedbe2f415e62f2655a68f4ee79698b63e4
bhardwajsaurav <saurav.bhardwaj@ekaleido.co>
2025-01-09 00:32:54 +0530

    update ui

--- api/index.js
+++ api/index.js
@@ -7,16 +7,16 @@ const app = express();
 const PORT = 3000;
 
 // Middleware to serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set view engine to EJS
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '../views'));
 
 // Function to check Employee ID in Excel
 function getSeatNumber(employeeId) {
-    const workbook = xlsx.readFile(path.join(__dirname, 'seatPage.xlsx'));
+    const workbook = xlsx.readFile(path.join(__dirname, '../seatPage.xlsx'));
     const sheet = workbook.Sheets[workbook.SheetNames[0]];
     const data = xlsx.utils.sheet_to_json(sheet);
     for (const row of data) {
@@ -29,7 +29,7 @@ function getSeatNumber(employeeId) {
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.post('/submit', (req, res) => {
